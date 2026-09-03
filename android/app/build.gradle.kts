545b0268328c139acf2c99f2e34a4e19f1ff6f96
gardenbylit <45648174+gardenbylit@users.noreply.github.com>
2026-09-03 13:29:47 +0700

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,3 +22,8 @@ dependencies {
 }
 
 implementation("com.google.android.gms:play-services-ads:25.4.0")
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    // ...
+    MobileAds.initialize(this)
+}
