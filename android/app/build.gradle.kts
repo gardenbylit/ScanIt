5b995f1d0ebb6545a9391e178fd3f098c6895d34
gardenbylit <45648174+gardenbylit@users.noreply.github.com>
2026-09-03 13:29:19 +0700

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,3 +20,5 @@ dependencies {
   // Add the dependencies for any other desired Firebase products
   // https://firebase.google.com/docs/android/setup#available-libraries
 }
+
+implementation("com.google.android.gms:play-services-ads:25.4.0")
